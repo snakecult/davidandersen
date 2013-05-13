78f8cefc7a07f33a41c9f14d48f44e5a6f68a32a
David Andersen <git@davidandersen.us>
2013-05-13 16:40:01 -0700

    add nextInt() and nextFloat()

--- da-util/src/main/java/us/davidandersen/random/Random.java
+++ da-util/src/main/java/us/davidandersen/random/Random.java
@@ -1,28 +1,31 @@
 package us.davidandersen.random;
 
-public class Random
-{
+public class Random {
 	private final java.util.Random random;
 
-	public Random()
-	{
+	public Random() {
 		this(new java.util.Random());
 	}
 
-	public Random(final java.util.Random random)
-	{
+	public Random(final java.util.Random random) {
 		this.random = random;
 	}
 
-	public int between(final int min, final int max)
-	{
+	public int between(final int min, final int max) {
 		final int num = random.nextInt(max - min + 1) + min;
 
 		return num;
 	}
 
-	public int nextInt(final int n)
-	{
+	public int nextInt() {
+		return random.nextInt();
+	}
+
+	public int nextInt(final int n) {
 		return random.nextInt(n);
+	}
+
+	public float nextFloat() {
+		return random.nextFloat();
 	}
 }
